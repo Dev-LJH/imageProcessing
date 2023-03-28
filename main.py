ea59c0a45740a244c17bf595974cc76b687e4c0f
JongHyeong <leejhjj90@gmail.com>
2023-03-28 17:41:42 +0900

    커밋1

---
+++ main.py
@@ -0,0 +1,18 @@
+import  cv2
+
+# 영상 소스 열기
+src = cv2.imread('images/picture01.jpg')
+
+### 영상 처리 알고리즘을 구현 ###
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+
+
+#############################
+
+
+# 영상 디스플레이
+cv2.imshow('src', src)
+
+# 마무리 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
